9afbbfa5f04aa2f3171cda1fe207338fe053ad37
Breno Luiz <brenoluizpv@gmail.com>
2026-04-28 09:20:29 -0300

    Criando configuração básica para rotas de usuários

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,5 +5,5 @@ const PORT = process.env.PORT || 3000;
 console.log("Iniciando servidor...");
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port http://localhost:${PORT}/api/`);
 });
--- backend/src/routes/routes.ts
+++ backend/src/routes/routes.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
+import usersRouter from '../modules/users/users.routes';
 
 const router = Router();
 
 router.get('/', (req, res) => {
   res.send('Hello, World!');
 });
+
+router.use('/users', usersRouter);
 
 export default router;
