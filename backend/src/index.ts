c937d1741e60495e9cec2714d41f77bcac381932
Breno Luiz <brenoluizpv@gmail.com>
2026-04-28 16:24:38 -0300

    Removendo uso desnecessário de validação de variaveis env em um arquivo incorreto

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,10 +5,6 @@ const PORT = process.env.PORT || 3000;
 
 console.log("Iniciando servidor...");
 
-if (!process.env.DB_NAME || !process.env.DB_USER || !process.env.DB_PASSWORD) {
-  throw new Error('Variáveis de ambiente do banco não definidas');
-}
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
