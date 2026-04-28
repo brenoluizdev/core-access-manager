9b1ba35e0701a76a1019e7d1e79ec9afce1a5f6a
Breno Luiz <brenoluizpv@gmail.com>
2026-04-28 16:10:54 -0300

    feat: criando conexão com o banco de dados postgreesql local

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,14 @@
 import express from 'express';
 import routes from './routes/routes';
+import sequelize from './config/database';
 
 const app = express();
 
 app.use(express.json());
 app.use('/api', routes);
+
+sequelize.authenticate()
+  .then(() => console.log('Database connected...'))
+  .catch(err => console.log('Error: ' + err));
 
 export default app;
